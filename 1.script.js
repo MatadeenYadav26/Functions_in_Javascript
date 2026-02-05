3f36164041e9fd65cf81e831adc4ed3f062327a9
KING <matadeenyadav202@gmail.com>
2026-02-05 07:17:31 +0530

    Create 1.script.js

---
+++ 1.script.js
@@ -0,0 +1,21 @@
+// functions in Javascript:
+
+// function sexkarna(){
+//     console.log("1.Kapde Utaro")
+//     console.log("2.For-Play Karo")
+//     console.log("3.Use Protection")
+//     console.log("4.Get into the right hole.")
+//     console.log("5.Go Slow , Go Deep , Go Better")
+//     console.log("6.Have a Great Blast")
+//     console.log("7.Stay Together , Have a moment...")
+//     console.log("7.Happy Ending")
+// }
+
+// sexkarna()
+
+// function dosomething(Name,Age,email){
+    // console.log(Name,Age,email);
+//     console.log(arguments);
+
+// }
+// dosomething("Aman",21,"aman@gmail.com");
